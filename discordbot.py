e6eaa119a781878f0a68bb38105e582b1b175094
kinotan <54130348+kinotan@users.noreply.github.com>
2019-08-15 14:27:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,20 @@
+    
 from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command＿prefix='/')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_message(message):
-    try:
-        if message.author.bot:
-            return
-        await bot.process_commands(message)
-     except Exception:
-        await message.channel.send(f'```\n{traceback.format_exc()}\n```')
-        @bot.commands
-        async def ping(ctx):
-            await ctx.send('pong')
-            
-            @bot.commands
-            async def kuso(ctx):
-                await ctx.send('wakaru')
-                
-                bot.run(token)
-            
-                   
-                   
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
+
+
+@bot.command()
+async def kuso(ctx):
+    await ctx.send('sine')
+
+
+bot.run(token)
