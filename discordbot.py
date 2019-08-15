1505c4f78deff7f793de75985e669ee84a78a3f2
kinotan <54130348+kinotan@users.noreply.github.com>
2019-08-15 15:13:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,3 @@
-
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-    
-@bot.command()
-async def sho(ctx):
-    await ctx.send('しね')
-
-bot.run(token)
-
 #discord.pyのインポート
 from asyncio import sleep
 import discord
