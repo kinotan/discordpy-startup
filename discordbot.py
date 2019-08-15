f9c84ee02140d53ae9f256fa58dfe37743d13bf2
kinotan <54130348+kinotan@users.noreply.github.com>
2019-08-15 14:50:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,3 @@
-    
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['kinotan']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-
-
-@bot.command()
-async def kuso(ctx):
-    await ctx.send('sine')
-
-
-bot.run(token)
-
 #discord.pyのインポート
 from asyncio import sleep
 import discord
